64d75fb680dc21d5fc409a666104026c61572255
behnoudng <behnoudng@gmail.com>
2024-12-20 23:53:14 +0330

    changes before theme change

--- game/utils.py
+++ game/utils.py
@@ -1,19 +1,27 @@
 import random, heapq
 from .models import Card
 
+time_heaps = {
+    'easy': [],
+    'normal': [],
+    'hard': []
+}
 
-def initialize_cards():
-    card_values = ["A", "B", "C", "D", "E", "F", "G", "H"]
+def initialize_cards(mode):
+    if mode == 'easy':
+        card_values = ["A","B","C","D"]  # 4 pairs
+    elif mode == 'hard':
+        card_values = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N"]  # 14 pairs
+    else:
+        card_values = ["A","B","C","D","E","F","G","H"]  # normal (8 pairs)
+
     deck = card_values * 2
     random.shuffle(deck)
-
     for value in deck:
         Card.objects.create(value=value)
 
+def add_time_record(time, mode):
+    heapq.heappush(time_heaps[mode], (time, f"{time}s"))
 
-time_heap = []
-def add_time_record(time):
-    heapq.heappush(time_heap, (time, f"{time}s"))
-
-def get_top_times(n=3):
-    return heapq.nsmallest(n, time_heap)
+def get_top_times(mode='normal', n=3):
+    return heapq.nsmallest(n, time_heaps[mode])
--- game/views.py
+++ game/views.py
@@ -7,20 +7,26 @@ from .utils import initialize_cards, get_top_times, add_time_record
 
 def submit_time_view(request):
     time = int(request.GET.get('time'))
-    add_time_record(time)
-    return JsonResponse({'status': 'success', 'top_records': get_top_times()})  
+    mode = request.GET.get('mode', 'normal')
+    add_time_record(time, mode)
+    return JsonResponse({'status': 'success', 'top_records': get_top_times(mode)})  
 
 def game_view(request):
-    if not Card.objects.exists():
-        initialize_cards()
+    mode = request.GET.get('mode', 'normal')
+    last_mode = request.session.get('last_mode')
+
+    if last_mode != mode:
+        Card.objects.all().delete()
+        initialize_cards(mode)
+        request.session['last_mode'] = mode
     
     cards = Card.objects.all()
-    top_records = get_top_times()
+    top_records = get_top_times(mode)
     
-    return render(request, 'game/game.html', {'cards': cards, 'top_records': top_records})
+    return render(request, 'game/game.html', {'cards': cards, 'top_records': top_records, 'mode': mode})
 
 
 
-  
-    
+
+
 
